b00dcc52d9e20aa149b237b109f43975dcfee214
Fred <frederique.bordeleau@gmail.com>
2020-01-13 19:41:00 +0000

    Importing eqArrays and exporting to middle and middleTest with @DragonMess and @Miaouchkinz

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require("./eqArrays")
+
 const assertArraysEqual = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
@@ -10,6 +12,10 @@ const assertArraysEqual = function (arr1, arr2) {
     return console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
 };
 
+
+module.exports = assertArraysEqual;
+/*
 assertArraysEqual([0, 3, 3], [1, 2, 3]); // => should PASS
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
 assertArraysEqual([1, 2, 3], [1, 2]); // => should PASS
+*/
