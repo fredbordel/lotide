dd208d7021b9bef8d68487088dd9c4499a14f983
Fred <frederique.bordeleau@gmail.com>
2020-01-13 19:40:14 +0000

    Deleting extra functions (importing eqArray and assertArray) and exporting to test file with @DragonMess and @Miaouchkinz

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
-   } 
-  }
-  return true
-};
-
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertArraysEqual = require("./assertArraysEqual")
 
 
 let middle = function (arrToFind){
@@ -38,4 +19,8 @@ let middle = function (arrToFind){
     }
   }
 };
-console.log(middle([1]));
+
+
+
+module.exports = middle;
+
