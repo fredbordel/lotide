f62165be6bac470bcec001b5b15721e0ada415d5
Fred <frederique.bordeleau@gmail.com>
2020-01-13 18:13:33 +0000

    Removed tests and move them to new testfile with @Miaouchkinz and @DragonMess

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require("./assertEqual")
 
 const tail = function (words) {
   return words.slice(1);
@@ -15,3 +8,5 @@ const words = ["Lighthouse", "Labs", "hey"];
 tail(words);
 assertEqual(words.length, 3);
 console.log(tail(["Hello", "Words", "Okay", 1, 3, 4]));
+
+module.exports = tail;
