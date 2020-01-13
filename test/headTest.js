d3b7106114717ee3a3f48c5756b30035bd6f3386
Fred <frederique.bordeleau@gmail.com>
2020-01-13 20:46:25 +0000

    Modifying headTest so that it works for chai

---
+++ test/headTest.js
@@ -0,0 +1,19 @@
+const assertEqual = require('../assertEqual');
+const head = require('../Head');
+const assert = require('chai').assert;
+
+
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), "Hello"); 
+  });
+  it("returns '5' for [5]", () => {
+    assert.strictEqual(head(['5']), '5')
+  })
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined)
+  })
+});
